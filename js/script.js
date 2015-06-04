f8e75146b9397504e5b685ac8bbbafb57026006d
Yusuke Hashimoto <link5199@gmail.com>
2015-06-04 21:53:17 +0900

    特定サイトのブロック、埋め込みYoutubeの非表示機能を作成

---
+++ js/script.js
@@ -0,0 +1,13 @@
+﻿//alert("ニコニコ動画なんか見てないで研究しなさい");
+//location.href="https://scholar.google.co.jp/schhp?hl=ja";
+//location.href="chrome/content/tasklist.html";
+//location.href="chrome-extension://chkckjbgpgbeljgecnabkolgniljocnk/tasklist.html";
+
+for(i = 0; i < document.getElementsByTagName("iframe").length; i++) {
+    //document.getElementsByTagName("iframe")[i].contentDocument.location.replace('');
+    document.getElementsByTagName("iframe")[i].style.display = "none";
+}
+
+if(location.href.includes("nicovideo") || location.href.includes("youtube")) {
+    location.href="chrome-extension://nhdenaocdammdiakijiiehkfjkongiel/tasklist.html";
+}
